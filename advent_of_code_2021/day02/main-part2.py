1b84911e9fe597b382384735a12f9244d6d73329
Calebe Maciel <calebe.maciel@care.com>
2021-12-02 12:09:01 -0500

    clean up a bit

--- advent_of_code_2021/day02/main-part2.py
+++ advent_of_code_2021/day02/main-part2.py
@@ -1,19 +1,17 @@
-with open('input.txt') as file:
-    commands = file.readlines()
-    commands = [line.rstrip() for line in commands]
-
 horizontal = 0
 depth = 0
 aim = 0
 
-for command in commands:
-    if command.startswith('forward'):
-        value = int(command.split(' ')[1])
-        horizontal += value
-        depth += aim * value
-    if command.startswith('down'):
-        aim += int(command.split(' ')[1])
-    if command.startswith('up'):
-        aim -= int(command.split(' ')[1])
+with open('input.txt') as commands:
+    for command in commands:
+        command = command.strip()
+        if command.startswith('forward'):
+            value = int(command.split()[1])
+            horizontal += value
+            depth += aim * value
+        if command.startswith('down'):
+            aim += int(command.split()[1])
+        if command.startswith('up'):
+            aim -= int(command.split()[1])
 
 print(horizontal * depth)
